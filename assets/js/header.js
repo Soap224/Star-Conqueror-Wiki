1d7117d4c2ebde0a747bd79c8283550b0af20a06
maingocphong176@gmail.com <maingocphong176@gmail.com>
2023-02-25 15:35:52 +0700

    Basic mobile interface

---
+++ assets/js/header.js
@@ -0,0 +1,11 @@
+function close_nav(){
+  var nav = document.querySelector('#nav')
+  console.log("thu gon headre")
+  nav.classList.toggle('close')
+};
+
+;(function star(){
+    var nav = document.querySelector('#nav')
+    console.log("thu gon header")
+    nav.classList.toggle('close')
+  })()
